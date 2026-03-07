70b5e0ac133605462944466d9d07560b44ef5551
Erfan <106490629+Erfan-TAV@users.noreply.github.com>
2026-03-07 03:25:23 -0800

    doxy comments campusStructs.h

--- headers/campusStructs.h
+++ headers/campusStructs.h
@@ -1,29 +1,43 @@
-//
-// Created by erfan on 2/25/2026.
-//
+/**
+* @file campusStructs.h
+ * @brief Definition of a campus object
+ * @author Erfan Tavassoli
+ */
 
 #ifndef COLLEGETOUR_CAMPUSSTRUCTS_H
 #define COLLEGETOUR_CAMPUSSTRUCTS_H
 #include <qlist.h>
 #include <QString>
 
+/**
+ * @struct campusSouvenir
+ * @brief Represents a single souvenir item available at a campus.
+ */
 struct campusSouvenir {
-    QString name;
-    double price;
+    QString name;   /**< The name of the souvenir item */
+    double price;   /**< The cost of the souvenir in USD */
 };
 
+/**
+ * @struct CampusDistances
+ * @brief Stores the distance relationship between the parent campus and another campus.
+ */
 struct CampusDistances {
-    int otherCampusID;
-    double distance;
+    int otherCampusID; /**< The campusID of the other campus */
+    double distance;   /**< The travel distance between the two campuses */
 };
 
+/**
+ * @struct Campus
+ * @brief A comprehensive container for all data related to a single college campus.
+ * * This struct aggregates the campus identity, its full inventory of souvenirs,
+ * and its network of distances to other colleges.
+ */
 struct Campus {
-    int campusID;
-    QString campusName;
-    QList<CampusDistances> distances;
-    QList<campusSouvenir> souvenirs;
+    int campusID;                       /**< campusID for the campus */
+    QString campusName;                 /**< The name of the college */
+    QList<CampusDistances> distances;   /**< QList of distance connections to other campuses */
+    QList<campusSouvenir> souvenirs;    /**< QList of all souvenirs available at this location */
 };
-
-
 
 #endif //COLLEGETOUR_CAMPUSSTRUCTS_H
