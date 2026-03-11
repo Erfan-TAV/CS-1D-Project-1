1e68f62befb4c3d41bd869da19098771b98bbcd4
ShahobCode <shahob.coder@gmail.com>
2026-03-11 13:34:51 -0700

    Update databasePage.h

--- headers/databasePage.h
+++ headers/databasePage.h
@@ -1,16 +1,36 @@
+/**
+ * @file databasePage.h
+ * @brief Base class for UI pages requiring database synchronization.
+ * @author Erfan Tavassoli
+ */
+
 #ifndef DATABASEPAGE_H
 #define DATABASEPAGE_H
 
 #include <QWidget>
 
-// This is the parent class for ANY page that needs to refresh when the DB changes
+/**
+ * @class DatabasePage
+ * @brief An abstract base class for all pages that display database-driven content.
+ * @details This class serves as an interface to ensure that all sub-pages 
+ * (like Admin, View Campuses, etc.) implement a standardized refresh mechanism 
+ * to stay in sync with the SQLite database.
+ */
 class DatabasePage : public QWidget {
     Q_OBJECT
 public:
+    /**
+     * @brief Constructor for DatabasePage.
+     * @param parent Pointer to the parent QWidget.
+     */
     explicit DatabasePage(QWidget *parent = nullptr) : QWidget(parent) {}
 
-    // Every child page MUST implement this function
+    /**
+     * @brief Pure virtual function to update the UI with the latest database data.
+     * @details Every child class must implement this method to reload its models, 
+     * refresh table views, or update labels whenever a change occurs in the database.
+     */
     virtual void refreshUI() = 0;
 };
 
-#endif
+#endif // DATABASEPAGE_H
